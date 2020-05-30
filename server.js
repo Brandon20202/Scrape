533cc3edd2d4e50ca89110f12080b29d94be7f13
Brandon20202 <brandon.taylor@austinisd.org>
2020-05-30 13:08:23 -0500

    finished code and is running

---
+++ server.js
@@ -0,0 +1,113 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+if (process.env.MONGODB_URI) {
+  mongoose = mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose = mongoose.connect("mongodb://localhost/scrape");
+}
+// Routes
+
+// A GET route for scraping the website
+app.get("/", function(req, res) {
+
+  axios.get("https://www.nytimes.com/section/us").then(function(response) {
+    
+    var $ = cheerio.load(response.data);
+
+  
+    $("article h2").each(function(i, element) {
+     
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+
+    res.send("Scrape Complete");
+  });
+});
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+app.get("/notes", function(req,res){
+  db.Note.find({}).then(function(dbNote){
+    res.json(dbNote);
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+     
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
